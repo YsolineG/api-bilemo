99cfbaec5085710bcf5d85c665619eeb47af7c59
YsolineG <ysoline.ganster@gmail.com>
2022-04-04 09:59:09 +0200

    feat: add methods register and login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/api/login_check', name: 'api_login')]
+    public function apiLogin(): Response
+    {
+        $user = $this->getUser();
+
+        return new Response([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+
+    #[Route('/api/register', name: 'api_register', methods: 'POST')]
+    public function register(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, Request $request, SerializerInterface $serializer): \Symfony\Component\HttpFoundation\JsonResponse
+    {
+        $json = $request->getContent();
+
+        $user = $serializer->deserialize($json, User::class, 'json');
+        $plaintextPassword = $user->getPassword();
+
+        $user->setPassword($passwordHasher->hashPassword($user, $plaintextPassword));
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'status' => 201,
+            'message' => "L'utilisateur a bien été ajouté"
+        ], 201);
+    }
+}
